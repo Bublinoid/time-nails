5d32522b38f0162a4a450c8a389d1daf2d757512
Hleb <gleb.qb@gmail.com>
2024-07-30 16:40:53 +0200

    init

---
+++ src/main/java/ru/bublinoid/timenails/TimeNailsApplication.java
@@ -0,0 +1,13 @@
+package ru.bublinoid.timenails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeNailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeNailsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bublinoid/timenails/TimeNailsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bublinoid.timenails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeNailsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
